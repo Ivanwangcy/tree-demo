d624f536a8f956661e656f1e99d2190b91ab02e1
Ivan <wcy5211678@163.com>
2016-10-20 14:17:18 +0800

    go back

--- src/components/UserDetailView/index.js
+++ src/components/UserDetailView/index.js
@@ -3,7 +3,7 @@ import Vue from "vue"
 export default {
   render(h) {
     return (
-      <img src="/dist/assets/S61019-224406(1).jpg" style={{width: '100vw', height: '100vh'}}></img>
+      <img on-click={() => {history.back()}} src="/dist/assets/S61019-224406(1).jpg" style={{width: '100vw', height: '100vh'}}></img>
     )
   }
 }
